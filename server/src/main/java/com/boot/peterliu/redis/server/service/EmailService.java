24fb9bc70faf9b5dc4135f5f79d106a149a7f74e
peterliu <3255635927@qq.com>
2022-02-25 18:52:51 +0800

    数据类型List~场景实战之多线程-发送邮件(二)

--- server/src/main/java/com/boot/peterliu/redis/server/service/EmailService.java
+++ server/src/main/java/com/boot/peterliu/redis/server/service/EmailService.java
@@ -3,7 +3,13 @@ package com.boot.peterliu.redis.server.service;
 import com.boot.peterliu.redis.model.entity.Notice;
 import com.boot.peterliu.redis.model.entity.User;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @Author: PeterLiu
@@ -13,10 +19,31 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    private Environment environment;
 
     //TODO:给指定的用户发送通告
-    public void emailNotificationToUsers(Notice notice, User user){
-        log.info("给指定用户:{} 发送通告:{}",user,notice);
+    public void emailNotificationToUsers(Notice notice, User user) {
+        log.info("给指定用户:{} 发送通告:{}", user, notice);
+        this.sendSimpleEmail(notice.getTitle(), notice.getContent(), user.getEmail());
+    }
+
+    //TODO:发送简单的邮件消息
+    public void sendSimpleEmail(final String subject, final String content, final String... toWhom) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(subject);
+            message.setText(content);
+            message.setFrom(environment.getProperty("mail.send.from"));//发送者
+            message.setTo(toWhom);//接收者
+
+            mailSender.send(message);
+            log.info("发送邮箱完毕");
+        } catch (Exception e) {
+            log.info("发送邮件消息-发生异常：{}",e.fillInStackTrace());
+        }
     }
 
 
